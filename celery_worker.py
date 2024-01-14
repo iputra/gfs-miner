5db664b6f5e02f04165fa25e70ed729540af8b8a
Ikhsan P <ikhsanputra@live.com>
2024-01-14 13:00:40 +0000

    chore: add celer_broker_url in config.yaml

--- celery_worker.py
+++ celery_worker.py
@@ -1,2 +1,8 @@
+import os
 from celery import Celery
-app = Celery('my_tasks', broker='redis://localhost:6379/0')
+from utils import read_yaml_config
+
+base_path = os.path.abspath('')
+config = read_yaml_config(f"{base_path}/config.yaml")
+
+app = Celery('my_tasks', broker=config["gfs-miner"]["celery_broker_url"])
